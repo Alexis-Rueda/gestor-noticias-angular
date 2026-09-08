5560990329c74302056742e3622c317d23873205
alexis.rueda <alexisrueda34@gmail.com>
2026-09-07 23:48:20 -0600

    feat: agregar configuración de Angular Schematics y crear interfaz Noticia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NoticiaPage } from './noticias/pages/noticia/noticia-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NoticiaPage
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/noticias/interfaces/noticia.ts
@@ -0,0 +1,8 @@
+
+export interface Noticia {
+  id: number;
+  titulo: string;
+  contenido: string;
+  imagenUrl: string;
+  autor: string;
+}
